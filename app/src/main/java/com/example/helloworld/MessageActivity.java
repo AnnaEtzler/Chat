21f3cf75aba7d4ba7016457ac19b24b1bac77d3f
Anna <anna.piskunova@web.de>
2022-07-14 21:39:28 +0200

    3 commit

--- app/src/main/java/com/example/helloworld/MessageActivity.java
+++ app/src/main/java/com/example/helloworld/MessageActivity.java
@@ -1,13 +1,23 @@
 package com.example.helloworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,7 +29,7 @@ public class MessageActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private ImageView imgToolbar, imgSend;
 
-    //private MessageAdapter messageAdapter;
+    private MessageAdapter messageAdapter;
     private ArrayList<Message> messages;
 
     String usernameOfTheRoommate, emailOfRoommate, chatRoomId;
@@ -29,7 +39,7 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        usernameOfTheRoommate = getIntent().getStringExtra("usermame_of_roommate");
+        usernameOfTheRoommate = getIntent().getStringExtra("username_of_roommate");
         emailOfRoommate = getIntent().getStringExtra("email_of_roommate");
 
         recyclerView = findViewById(R.id.recycleMessages);
@@ -38,12 +48,80 @@ public class MessageActivity extends AppCompatActivity {
         txtChattWith = findViewById(R.id.txtChattingWith);
         progressBar = findViewById(R.id.progressMesages);
         imgToolbar = findViewById(R.id.img_toolbar);
-
-
-
-
-      txtChattWith.setText(usernameOfTheRoommate);
-
+        txtChattWith.setText(usernameOfTheRoommate);
         messages = new ArrayList<>();
+
+        imgSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase.getInstance("https://helloworld-a6ce6-default-rtdb.europe-west1.firebasedatabase.app").getReference("messages/" + chatRoomId).push().setValue(new Message(FirebaseAuth.getInstance().getCurrentUser().getEmail(), emailOfRoommate,  edtMessageInput.getText().toString()));
+                edtMessageInput.setText("");
+
+            }
+        });
+
+        messageAdapter = new MessageAdapter(messages, getIntent().getStringExtra("my_img"), getIntent().getStringExtra("img_of_roommate"), MessageActivity.this);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(messageAdapter);
+        Glide.with(MessageActivity.this).load(getIntent().getStringExtra("img_of_roommate")).placeholder(R.drawable.account_img).error(R.drawable.account_img).into(imgToolbar);
+
+
+
+
+
+        setUpChatRoom();
+    }
+
+
+    private void setUpChatRoom(){
+
+        FirebaseDatabase.getInstance("https://helloworld-a6ce6-default-rtdb.europe-west1.firebasedatabase.app").getReference("user/" + FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override  //nen
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String myUsername = snapshot.getValue(User.class).getUsername();
+                if(usernameOfTheRoommate.compareTo(myUsername)>0){
+                    chatRoomId = myUsername + usernameOfTheRoommate;
+                } else if (usernameOfTheRoommate.compareTo(myUsername) == 0){
+                    chatRoomId = myUsername + usernameOfTheRoommate;
+                } else {
+                    chatRoomId = usernameOfTheRoommate + myUsername;
+                }
+
+                attacMessageListener(chatRoomId);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void attacMessageListener(String chatRoomId){
+        FirebaseDatabase.getInstance("https://helloworld-a6ce6-default-rtdb.europe-west1.firebasedatabase.app").getReference("messages/" + chatRoomId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messages.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    messages.add(dataSnapshot.getValue(Message.class));
+                }
+
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.scrollToPosition(messages.size() - 1);
+                recyclerView.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
